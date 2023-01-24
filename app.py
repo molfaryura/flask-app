970a69306eb41d289cd9a87882f417fe133389b6
Oleksandr <oleksandrr1996@protonmail.com>
2023-01-24 22:31:50 +0200

    Create functions to store data

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -29,6 +29,26 @@ class Author(db.Model):
     fact = db.relationship('Facts', backref='author')
 
 
+def post_fact(template, page):
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['author']
+        author = request.form['author']
+                                                        
+        facts_data = Facts(title=title,
+                        text=text)
+
+        author_data = Author(name=author)
+
+        try:
+            db.session.add_all([facts_data, author_data])
+            db.session.commit()
+            redirect(page)
+        except:
+            return 'Failed to connect to the database :('
+    return render_template(template)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -37,17 +57,25 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/shavkoon')
+def shavk():
+    return render_template('post_shavk.html')
+
+@app.route('/vasyl')
+def vasyl():
+    return render_template('post_vasyl.html')
+
 @app.route('/donate')
 def donate():
     return render_template('donate.html')
 
-@app.route('/post/shavk', methods=['POST', 'GET'])
+@app.route('/post/shavkoon', methods=['POST', 'GET'])
 def post_shavk():
-    return render_template('post_shavk.html')
+    return post_fact('post_shavk.html', '/shavkoon')
 
 @app.route('/post/vasyl', methods=['POST', 'GET'])
 def post_vasyl():
-    return render_template('post_vasyl.html')
+    return post_fact('post_vasyl.html','/vasyl')
 
 if __name__ == '__main__':
     app.run(debug=True)
