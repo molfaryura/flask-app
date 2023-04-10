14840a341ffb9912fd972c6f56c93eca79d10175
molfaryura <oleksandrr1996@protonmail.com>
2023-04-10 17:51:15 +0300

    Fixed code style issues

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-'''
-WebApp on Flask and SQLAlchemy.
-In this app the user can store and get 
-data from the database about two
-people, who are widely known in
-narrow circles
-'''
+"""WebApp on Flask and SQLAlchemy. In this app the user can store and get
+data from the database about two people, who are widely known in narrow circles
+"""
 
 import os
 
@@ -14,7 +10,7 @@ from dotenv import load_dotenv
 
 from flask_login import login_required, current_user, login_user, logout_user
 
-from models import *
+from models import Facts, Users, login, db
 
 load_dotenv()
 
@@ -39,10 +35,21 @@ second_answer = os.environ.get('Second_answer')
 
 @app.before_first_request
 def create_table():
+    """Creates table if not exists"""
     db.create_all()
 
 
 def post_fact(template, page):
+    """ Handle POST request to post a fact to the database.
+
+    param template (str): The name of the template to render if the request method is not POST.
+    param page (str): The URL path to redirect to after successfully posting a fact.
+
+    returns:
+             If the request method is not POST, render the specified template.
+             If successfully posted a fact, redirect to the specified page.
+             If failed to connect to the database, return an error message.
+    """
     if request.method == 'POST':
         title = request.form['title']
         text = request.form['text']
@@ -57,7 +64,7 @@ def post_fact(template, page):
         try:
             db.session.add(facts_data)
             db.session.commit()
-            redirect(page)
+            return redirect(page)
         except:
             return 'Failed to connect to the database :('
     return render_template(template)
@@ -65,31 +72,49 @@ def post_fact(template, page):
 
 @app.route('/')
 def index():
+    """Returns the main page"""
     return render_template('index.html')
 
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
+    """ Handle user login.
+
+    returns:
+        redirect: If the user is already authenticated, redirect to the home page.
+        redirect: If the request method is POST and the email and password are correct, redirect to the home page.
+        redirect: If the email or password is incorrect, redirect back to the login page with an error flash message.
+        render_template: If the request method is not POST, render the login template.
+    """
     if current_user.is_authenticated:
         return redirect('/')
-     
+
     if request.method == 'POST':
         email = request.form['email']
-        user = UserModel.query.filter_by(email=email).first()
+        user = Users.query.filter_by(email=email).first()
         if user is not None and user.check_password(request.form['password']):
             login_user(user)
             return redirect('/')
-        
-        else:
-            flash('Email or Password is incorrect!')
-            return redirect('/login')
-     
+
+        flash('Email or Password is incorrect!')
+        return redirect('/login')
+
     return render_template('login.html')
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
+    """ Handle user registration.
+
+    returns:
+        redirect: If the user is already authenticated, redirect to the home page.
+        redirect: If the request method is POST and the registration is successful, redirect to the home page.
+        redirect: If a user with the same email already exists, redirect back to the registration page with an error flash message.
+        redirect: If the passwords do not match, redirect back to the registration page with an error flash message.
+        redirect: If the security question answer is invalid, redirect to a YouTube link (for humorous purposes).
+        render_template: If the request method is not POST, render the registration template.
+    """
     if current_user.is_authenticated:
         return redirect('/')
-     
+
     if request.method == 'POST':
         email = request.form['email']
         username = request.form['username']
@@ -97,19 +122,19 @@ def register():
         repeat_password = request.form['repeat_password']
         question = request.form['question']
 
- 
-        if UserModel.query.filter_by(email=email).all():
+
+        if Users.query.filter_by(email=email).all():
             flash('A user with this email already exists')
             return redirect('/register')
-        
+
         if password != repeat_password:
             flash('Passwords must match')
             return redirect('/register')
-        
-        if question != first_answer and question != second_answer:
+
+        if question not in (first_answer, second_answer):
             return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-        
-        user = UserModel(email=email, username=username)
+
+        user = Users(email=email, username=username)
         user.set_password(password)
         db.session.add(user)
         db.session.commit()
@@ -118,69 +143,80 @@ def register():
 
 @app.route('/about')
 def about():
+    """Return about page"""
     return render_template('about.html')
 
 @app.route('/donate')
 def donate():
+    """Return donate page"""
     return render_template('donate.html')
 
 @app.route('/post/shavkoon', methods=['POST', 'GET'])
 @login_required
 def post_shavk():
+    """Return post_shavk.html page"""
     return post_fact('post_shavk.html', '/post/shavkoon')
 
 @app.route('/post/vasyl', methods=['POST', 'GET'])
 @login_required
 def post_vasyl():
+    """Return post_vasyl.html page"""
     return post_fact('post_vasyl.html','/post/vasyl')
 
 @app.route('/resource')
 def resource():
+    """Return resource page"""
     return render_template('resource.html')
 
 @app.route('/team')
 def team():
+    """Return team page"""
     return render_template('team.html')
 
 @app.route('/contact')
 def contact():
+    """Return contact page"""
     return render_template('contact.html')
 
 @app.route('/read', methods=['POST', 'GET'])
 def read():
-    '''
-    The user selects a person about whom 
-    he wants to get facts.
-    It connects to the database and 
-    show all facts on the separate html page
-    '''
+    """Handle GET and POST requests for the '/read' URL.
+
+    returns:
+        response:
+            Rendered HTML template as response for GET requests,
+            or filtered data rendered in result.html template for POST requests.
+    """
 
     if request.method == 'GET':
         return render_template('read.html')
-    elif request.method == 'POST':
-        if request.form.get("person") == 'All':
-            db_data = Facts.query.all()
-            return render_template('result.html', db_data=db_data)
-        else:
-            person = request.form.get("person")
-            db_data = Facts.query.filter_by(person = person).all()
-            return render_template('result.html', db_data=db_data)
+    if request.method == 'POST' and request.form.get("person") == 'All':
+        db_data = Facts.query.all()
+        return render_template('result.html', db_data=db_data)
+    person = request.form.get("person")
+    db_data = Facts.query.filter_by(person = person).all()
+    return render_template('result.html', db_data=db_data)
 
-@app.route('/read/<int:id>')
-def read_more(id):
-    '''
-    Return html page where url contains
-    primary key for particular record 
-    in the database. On that html page
-    the user will see a full text of
-    a certain fact
-    '''
+@app.route('/read/<int:page_id>')
+def read_more(page_id):
+    """Handle GET requests for the '/read/<page_id>' URL.
 
-    db_data = Facts.query.filter_by(id=id).first()
+    param page_id (int): The page ID to retrieve data from the database.
+
+    returns:
+        response: Rendered HTML template with data retrieved from the database.
+    """
+
+    db_data = Facts.query.filter_by(id=page_id).first()
     return render_template("read_more.html", db_data=db_data)
 
 @app.route('/logout')
 def logout():
+    """Handle GET requests for the '/logout' URL.
+
+    Returns:
+        response: Redirects to the home page ('/') after logging out the user.
+    """
     logout_user()
     return redirect('/')
 
--- models.py
+++ models.py
@@ -1,7 +1,5 @@
-'''Database structure with two
-    tables for facts and user's
-    credentials with one to many
-    relationship
+'''Database structure with two tables for facts and user's
+credentials with one to many relationship
 '''
 
 from flask_sqlalchemy import SQLAlchemy
@@ -10,37 +8,70 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from flask_login import UserMixin, LoginManager
 
-
 login = LoginManager()
-
 
 db = SQLAlchemy()
 
 class Facts(db.Model):
+    """ Model for the 'facts' table in the database.
+
+    Attributes:
+        id (int): Unique identifier for a fact.
+        title (str): Title of the fact.
+        text (str): Text of the fact.
+        author_id (int): Foreign key referencing the 'id' of the user who authored the fact.
+        person (str): Name of the person to whom the fact pertains.
+    """
+
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable = False)
+    title = db.Column(db.String(255), nullable = False)
     text = db.Column(db.Text, nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    person = db.Column(db.String, nullable=False)
+    person = db.Column(db.String(255), nullable=False)
 
-class UserModel(UserMixin, db.Model):
-    '''Create table for 
-       the registration
-    '''
+class Users(UserMixin, db.Model):
+    """ Model for the 'users' table in the database.
+
+    Attributes:
+        id (int): Unique identifier for a user.
+        email (str): Email address of the user. Must be unique.
+        username (str): Username of the user.
+        password_hash (str): Hashed password of the user.
+        fact (relationship): One-to-many relationship with the 'Facts' model.
+    """
+
     __tablename__ = 'users'
- 
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(80), unique=True, nullable = False)
     username = db.Column(db.String(100), nullable = False)
-    password_hash = db.Column(db.String(), nullable = False)
+    password_hash = db.Column(db.String(128), nullable = False)
     fact = db.relationship('Facts', backref='author')
- 
+
     def set_password(self,password):
+        """Set the password for the user.
+
+        param password (str): Plain text password.
+
+        returns: None
+        """
         self.password_hash = generate_password_hash(password)
-     
+
     def check_password(self,password):
+        """Check if the provided password matches the stored password hash.
+
+        param password (str): Plain text password to check against the stored hash.
+
+        returns: bool
+        """
         return check_password_hash(self.password_hash,password)
-    
+
 @login.user_loader
-def load_user(id):
-    return UserModel.query.get(int(id))
+def load_user(user_id):
+    """ Load a user from the database based on the provided user ID.
+
+    param user_id (int): The ID of the user to load.
+
+    returns: The User object associated with the provided user ID, or None if not found.
+    """
+    return Users.query.get(int(user_id))
